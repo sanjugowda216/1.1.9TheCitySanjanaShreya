f243925c48ad3d5e28a2999a77de2061b8a2158e
Sanjana Gowda <sanjanagowda@Sanjanas-MacBook-Air.local>
2024-10-15 19:22:29 -0700

    yay buildings

--- city.py
+++ city.py
@@ -1,18 +1,17 @@
 import turtle
 import random
 t = turtle.Turtle()
-building_height = [150,160,170,175,180,190,200,210,230,350,390,400]
-building_width = [45,47,49,50,55,60,80,85,90,95,97,100]
+building_gradient = ['black','gray','darkgray','lightgray']
+building_height = [150,160,170,175,180,190,200,210,230,350,390,210,230,350,390,400]
+building_width = [45,47,49,50,55,60,80,82,45,47,49,50,55,60,80,82,85,90,95,97]
 # sanjana
 def building():
     global building_height
     global building_width
     for i in range(1):
         height = random.choice(building_height)
-        building_height.remove(height)
 
         width = random.choice(building_width)
-        building_width.remove(width)
 
         t.forward(height)
         t.right(90)
@@ -21,20 +20,31 @@ def building():
         t.forward(height)
         
 # sanjana
+
 def farbuildings():
+    global building_gradient
+    t.speed(0)
     t.penup()
-    t.goto(-400,-100)
+    t.goto(-400,-120)
     t.pendown()
+    t.pensize(0)
     t.setheading(90)
-
-    t.fillcolor("black")
-    t.begin_fill()
-
-    for b in range(11):
-        t.setheading(90)
-        building()
+    for row in range(4):
         
-    t.end_fill()
+
+        pop = building_gradient.pop()
+        t.fillcolor(pop)
+        t.begin_fill()
+
+        for b in range(15):
+            t.setheading(90)
+            building()
+            
+        t.end_fill()
+        t.penup()
+        t.goto(-400, t.ycor() - 50) 
+        t.pendown()
+        t.setheading(90)
 
 farbuildings()
 turtle.speed(0)
